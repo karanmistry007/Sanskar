a1ae45d521735b052bc094ef4d9177a29c314aaa
karanmistry007 <ksmistry007@gmail.com>
2023-08-01 16:37:29 +0530

    commit

---
+++ Tailwind-Elixir/main.js
@@ -0,0 +1,28 @@
+// for dropdown menu 
+dropdown1=document.querySelector(".dropdown1");
+dropitem1=document.querySelector(".dropdown-item1");
+
+dropdown1.addEventListener("mouseover", function(){ dropitem1.style.display="flex"});
+dropdown1.addEventListener("mouseleave", function(){ dropitem1.style.display="none"});
+
+
+dropdown2=document.querySelector(".dropdown2");
+dropitem2=document.querySelector(".dropdown-item2");
+
+dropdown2.addEventListener("mouseover", function(){ dropitem2.style.display="flex"});
+dropdown2.addEventListener("mouseleave", function(){ dropitem2.style.display="none"});
+
+
+dropdown3=document.querySelector(".dropdown3");
+dropitem3=document.querySelector(".dropdown-item3");
+
+dropdown3.addEventListener("mouseover", function(){ dropitem3.style.display="flex"});
+dropdown3.addEventListener("mouseleave", function(){ dropitem3.style.display="none"});
+
+
+dropdown4=document.querySelector(".dropdown4");
+dropitem4=document.querySelector(".dropdown-item4");
+
+dropdown4.addEventListener("mouseover", function(){ dropitem4.style.display="flex"});
+dropdown4.addEventListener("mouseleave", function(){ dropitem4.style.display="none"});
+
