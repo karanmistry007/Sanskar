9ef2a75e088989aca835c4f5ad6ec44b39cba175
karanmistry007 <ksmistry007@gmail.com>
2023-08-01 10:42:45 +0530

    my third commit

---
+++ Bootstrap-Maxant/main.js
@@ -0,0 +1,141 @@
+// windows on scroll runs functions
+window.onscroll = function () {
+  scrollFunction();
+  activeColor();
+};
+
+// for navbar color and size
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    console.log(document.body.scrollTop);
+    console.log(document.documentElement.scrollTop);
+    document.getElementById("header").style.backgroundColor = "#2b3044";
+    document.getElementById("header").style.position = "fixed";
+    document.getElementById("header").style.animationName = "nav-animation";
+    document.querySelector(".logo").style.maxHeight = "60px";
+    // document.getElementById("logo").style.backgroundColor = "red"
+  } else {
+    document.getElementById("header").style.backgroundColor = "transparent";
+    document.getElementById("header").style.position = "absolute";
+    document.getElementById("header").style.animationName = "ease-in-out";
+    document.querySelector(".logo").style.maxHeight = "initial";
+    // document.getElementById("logo").style.backgroundColor = "red"
+  }
+}
+
+// for active area color
+function activeColor() {
+  var home = document.getElementById("home-btn");
+  var services = document.getElementById("services-btn");
+  var about = document.getElementById("about-btn");
+  var testimonials = document.getElementById("testimonials-btn");
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    home.classList.remove("text-white");
+    services.classList.add("text-white");
+    about.classList.add("text-white");
+    testimonials.classList.add("text-white");
+  }
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    home.classList.add("text-white");
+    services.classList.remove("text-white");
+    about.classList.add("text-white");
+    testimonials.classList.add("text-white");
+  }
+  if (
+    document.body.scrollTop > 1900 ||
+    document.documentElement.scrollTop > 1900
+  ) {
+    home.classList.add("text-white");
+    services.classList.add("text-white");
+    about.classList.remove("text-white");
+    testimonials.classList.add("text-white");
+  }
+  if (
+    document.body.scrollTop > 3400 ||
+    document.documentElement.scrollTop > 3400
+  ) {
+    home.classList.add("text-white");
+    services.classList.add("text-white");
+    about.classList.add("text-white");
+    testimonials.classList.remove("text-white");
+  }
+}
+
+// for display tables
+function table1() {
+  document.getElementById("btn-1").classList.add("orange");
+  document.getElementById("btn-2").classList.remove("orange");
+  document.getElementById("btn-3").classList.remove("orange");
+
+  document.getElementById("table1").style.display = "inline-table";
+  document.getElementById("table2").style.display = "none";
+  document.getElementById("table3").style.display = "none";
+}
+function table2() {
+  document.getElementById("btn-1").classList.remove("orange");
+  document.getElementById("btn-2").classList.add("orange");
+  document.getElementById("btn-3").classList.remove("orange");
+
+  document.getElementById("table1").style.display = "none";
+  document.getElementById("table2").style.display = "inline-table";
+  document.getElementById("table3").style.display = "none";
+}
+function table3() {
+  document.getElementById("btn-1").classList.remove("orange");
+  document.getElementById("btn-2").classList.remove("orange");
+  document.getElementById("btn-3").classList.add("orange");
+
+  document.getElementById("table1").style.display = "none";
+  document.getElementById("table2").style.display = "none";
+  document.getElementById("table3").style.display = "inline-table";
+}
+
+// for burger color
+const burger = document.getElementById("burger-cont");
+const btn1 = document.getElementById("line-1");
+const btn2 = document.getElementById("line-2");
+const btn3 = document.getElementById("line-3");
+
+let index = 0;
+
+const colors = ["#ff511a", "white"];
+// const l1 =["l1-a1","l1-a2"];
+// const l2 =["l2-a1","l2-a2"];
+// const l3 =["l3-a1","l3-a2"];
+
+//btn1
+const a1 = ["225deg", "360deg"];
+const p = ["absolute", "initial"];
+const top1 = ["8%", "initial"];
+//btn2
+const tra1 = ["250%", "0%"];
+const op = ["0", "1"];
+//btn3
+const a2 = ["135deg", "360deg"];
+
+burger.addEventListener("click", function onClick() {
+  btn1.style.backgroundColor = colors[index];
+  btn1.style.rotate = a1[index];
+  btn1.style.position = p[index];
+
+  btn1.style.top = top1[index];
+
+  btn2.style.backgroundColor = colors[index];
+  btn2.style.translate = tra1[index];
+  btn2.style.opacity = op[index];
+
+  btn3.style.backgroundColor = colors[index];
+
+  btn3.style.rotate = a2[index];
+  btn3.style.position = p[index];
+
+  btn3.style.top = top1[index];
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
---
+++ Tailwind-Growmark/main.js
@@ -0,0 +1,298 @@
+// slider js
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+
+//carousel cards
+const wrapper = document.querySelector(".wrapper");
+const carousel1 = document.querySelector(".carousel");
+const firstCardWidth = carousel1.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel1.children];
+
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel1.offsetWidth / firstCardWidth);
+
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens
+  .slice(-cardPerView)
+  .reverse()
+  .forEach((card) => {
+    carousel1.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
+
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach((card) => {
+  carousel1.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel1.classList.add("no-transition");
+carousel1.scrollLeft = carousel1.offsetWidth;
+carousel1.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel1.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel1.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel1.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel1.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel1.classList.remove("dragging");
+};
+
+const infiniteScroll = () => {
+  // If the carousel is at the beginning, scroll to the end
+  if (carousel1.scrollLeft === 0) {
+    carousel1.classList.add("no-transition");
+    carousel1.scrollLeft = carousel1.scrollWidth - 2 * carousel1.offsetWidth;
+    carousel1.classList.remove("no-transition");
+  }
+  // If the carousel is at the end, scroll to the beginning
+  else if (
+    Math.ceil(carousel1.scrollLeft) ===
+    carousel1.scrollWidth - carousel1.offsetWidth
+  ) {
+    carousel1.classList.add("no-transition");
+    carousel1.scrollLeft = carousel1.offsetWidth;
+    carousel1.classList.remove("no-transition");
+  }
+
+  // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+  clearTimeout(timeoutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+};
+
+const autoPlay = () => {
+  if (window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+  // Autoplay the carousel after every 2500 ms
+  timeoutId = setTimeout(() => (carousel1.scrollLeft += firstCardWidth), 2500);
+};
+autoPlay();
+
+carousel1.addEventListener("mousedown", dragStart);
+carousel1.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel1.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
+
+
+//dropdown menu
+function dropshow() {
+  document.getElementById("drop-item").style.display = "flex";
+  document.getElementById("drop-item").style.animation = "from-bottom 0.5s";
+}
+function drophide() {
+  document.getElementById("drop-item").style.animation="from-bottom-rev 0.5s";
+  setTimeout(() => {
+    document.getElementById("drop-item").style.display = "none";
+  }, 500);
+  
+  // setTimeout(()=>{document.getElementById("drop-item").style.display="none";},1000);
+  // setTimeout(document.getElementById("drop-item").style.display="none", 1000);
+
+}
+
+function dropclick() {
+  var x = document.getElementById("drop-item");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+    x.style.animation="from-bottom 0.5s";
+  } else {
+    x.style.animation="from-bottom-rev 0.5s";
+    setTimeout(() => {
+      x.style.display = "none";
+    }, 5000);
+  }
+}
+
+//navbar toogle
+function navToggle() {
+  var x = document.getElementById("nav-toggle");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+    x.style.animationName = "nav-toggle-1";
+  } else {
+    x.style.animationName = "nav-toggle-2";
+    setTimeout(() => {
+      x.style.display = "none";
+    }, 500);
+    
+  }
+}
+
+
+
+// grids animation
+const observerGrid = new IntersectionObserver((entriesGrid) => {
+  entriesGrid.forEach((entry) => {
+
+    if (entry.isIntersecting) {
+      entry.target.style.transform = "translate3d(0, 0, 0)";
+      entry.target.style.opacity = 1;
+    }
+    // else{
+    //   entry.target.style.transform="translate3d(-50px, 0, 0)";
+    //   entry.target.style.opacity = 0;
+    // }
+  })
+})
+
+const grids = document.querySelectorAll(".grid-item");
+grids.forEach((el) => observerGrid.observe(el));
+
+//services animation
+const observerSer = new IntersectionObserver((entriesSer) => {
+  entriesSer.forEach((entry) => {
+
+    if (entry.isIntersecting) {
+      entry.target.style.transform = "translate3d(0, 0, 0)";
+      entry.target.style.opacity = 1;
+    }
+    // else{
+    //   entry.target.style.transform="translate3d(0, 100px, 0)";
+    //   entry.target.style.opacity = 0;
+    // }
+  })
+})
+//services
+const services = document.querySelectorAll(".ser-item");
+services.forEach((el) => observerSer.observe(el));
+
+const serHead = document.querySelectorAll(".ser-head");
+serHead.forEach((el) => observerSer.observe(el));
+
+//project
+const projectHead = document.querySelectorAll(".project-head");
+projectHead.forEach((el) => observerSer.observe(el));
+
+const projectMain = document.querySelectorAll(".project-main");
+projectMain.forEach((el) => observerSer.observe(el));
+
+//about 
+const aboutText = document.querySelectorAll(".about-text");
+aboutText.forEach((el) => observerSer.observe(el));
+
+//quote
+const item1 = document.querySelectorAll(".q-item-1");
+item1.forEach((el) => observerSer.observe(el));
+
+const item2 = document.querySelectorAll(".q-item-2");
+item2.forEach((el) => observerSer.observe(el));
+
+//team
+const teamHead = document.querySelectorAll(".team-head");
+teamHead.forEach((el) => observerSer.observe(el));
+
+const teamItem = document.querySelectorAll(".team-item");
+teamItem.forEach((el) => observerSer.observe(el));
+
+//testimonial
+const testiHead = document.querySelectorAll(".testi-head");
+testiHead.forEach((el) => observerSer.observe(el));
+
+const testiMain = document.querySelectorAll(".testi-main");
+testiMain.forEach((el) => observerSer.observe(el));
+
+// footer
+const footer = document.querySelectorAll(".footer");
+footer.forEach((el) => observerSer.observe(el));
+
+
+
+
+
+//navbar positioning
+// windows on scroll runs functions
+window.onscroll = function () {
+  scrollFunction();
+};
+
+// for navbar color and size
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+
+    document.getElementById("header").style.position = "fixed";
+    document.getElementById("header").style.top = "0px";
+    document.getElementById("header").style.animationName = "nav-animation";
+  } else {
+    document.getElementById("header").style.position = "initial";
+    document.getElementById("header").style.animationName = "fade-1";
+  }
+}
+
+// for embeded video
+function modelDisplay(){
+  model=document.getElementById("model");
+  if (model.style.display === "none"){
+    model.style.display="flex";
+    model.style.animationName="from-top-2";
+  }else{
+    model.style.animationName="from-bottom-2";
+    setTimeout(() => {
+      model.style.display="none";
+    }, 500);
+    
+  }
+}
+
+
+
+
+
+
